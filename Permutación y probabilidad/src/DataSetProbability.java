57b279cb881f516cdc11d9ec428f66a60fb85bb8
Sneyder Cuéllar Martinez <162257935+Elsneyder@users.noreply.github.com>
2024-06-11 10:52:42 -0600

    Hands-8

---
+++ Permutación y probabilidad/src/DataSetProbability.java
@@ -0,0 +1,17 @@
+public class DataSetProbability {
+    private int[][] x1 = {{33, 22}, {25, 46}};
+
+    public DataSetProbability() {}
+
+    public int[][] getX1() {
+        return x1;
+    }
+
+    public void setX1(int[][] x1) {
+        this.x1 = x1;
+    }
+
+    public int[][] getDataset() {
+        return x1;
+    }
+}
---
+++ Permutación y probabilidad/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] chars = {'a', 'b', 'c'};
+
+        // Generar permutaciones
+        Permutation permutation = new Permutation(chars);
+        List<String> permutations = permutation.getPermutations();
+        System.out.println("Permutations:");
+        for (String perm : permutations) {
+            System.out.println(perm);
+        }
+
+        // Inicializar Datasets
+        DataSetPermutation datasetPermutation = new DataSetPermutation();
+        DataSetProbability datasetProbability = new DataSetProbability();
+
+        // Calcular probabilidades
+        Probability probability = new Probability(datasetPermutation, datasetProbability);
+        System.out.println("\nProbabilities:");
+
+        // Ejemplos de cálculo de probabilidades
+        System.out.println("P(A): " + probability.getMarginalProbability("a"));
+        System.out.println("P(B): " + probability.getMarginalProbability("b"));
+        System.out.println("P(C): " + probability.getMarginalProbability("c"));
+
+        System.out.println("P(A, B): " + probability.getJointProbability("a", "b"));
+        System.out.println("P(B, C): " + probability.getJointProbability("b", "c"));
+
+        System.out.println("P(A|B): " + probability.getConditionalProbability("a", "b"));
+        System.out.println("P(B|C): " + probability.getConditionalProbability("b", "c"));
+
+        // Obtener y mostrar el dataset de DataSetProbability
+        int[][] datasetProb = datasetProbability.getDataset();
+        System.out.println("\nDataSetProbability:");
+        for (int[] row : datasetProb) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        // Obtener y mostrar el dataset de DataSetPermutation
+        String[] datasetPerm = datasetPermutation.getDataset();
+        System.out.println("\nDataSetPermutation:");
+        System.out.println(Arrays.toString(datasetPerm));
+    }
+}
