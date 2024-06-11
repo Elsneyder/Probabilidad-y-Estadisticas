010dd8a3eff3953e763e4ffbaffb4df5eb161ebd
Sneyder Cuéllar Martinez <162257935+Elsneyder@users.noreply.github.com>
2024-06-11 11:39:29 -0600

    Hands-8

---
+++ Permutación y probabilidad/src/DataSetProbability.java
@@ -0,0 +1,17 @@
+public class DataSetProbability {
+    private int[][] x1 = {{69, 62}, {52, 30}};
+
+    public DataSetProbability() {}
+
+    public int[][] getX1() {
+        return x1;
+    }
+
+    public void setX1(int[][] x1) {
+        this.x1 = x1;
+    }
+
+    public int[][] getDataset() {
+        return x1;
+    }
+}
